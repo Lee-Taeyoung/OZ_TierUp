eb51829d7df3a0dc28fad1f0b03ff6441e97fcf5
이태영 <scammerlee1943@gmail.com>
2026-07-05 01:54:41 +0900

    [feat] 사운드 매니저 및 발소리 재생 추가

---
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    [SerializeField] private AudioSource _sfxSource;
+
+    [SerializeField] private float _sfxVolume = 0.5f;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        _sfxSource.volume = _sfxVolume;
+    }
+
+    public void PlaySFX(AudioClip clip, float volumeScale = 1f)
+    {
+        if (_sfxSource == null || clip == null)
+        {
+            return;
+        }
+        
+        _sfxSource.PlayOneShot(clip, _sfxVolume * volumeScale);
+    }
+}
